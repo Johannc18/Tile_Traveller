5468dcc14f446c6378a702c5c672c5950a43ed01
unknown <johannc18@ru.is>
2018-09-18 09:49:07 +0000

    Actual algorithm

--- tileTraveller.py
+++ tileTraveller.py
@@ -1,7 +1,15 @@
-"""
-heyt 
-harold yes 
-sir
-"""
+# first run through:
+# define movement limitations
+# create input statement for N E S W and convert to str
+#create while loop for N E S W
+#create for loop for printing options
+# use  if, elif, else statements
+# copy paste input statement
+# close while loop and print "Victory" for 3, 1 square
 
-print("Hello world")
+
+#after:
+
+
+# Create a function for movement
+# Create function for limiting movement
